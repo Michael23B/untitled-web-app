74bde6fd34f423f07b575c090121a7422d54e53a
yung lean <33080132+Michael23B@users.noreply.github.com>
2018-04-06 17:02:42 +1000

    untitled website
    (will have memes) :smirk:

---
+++ Web/Areas/Admin/Controllers/PagesController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace Web.Areas.Admin.Controllers
+{
+    public class PagesController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
