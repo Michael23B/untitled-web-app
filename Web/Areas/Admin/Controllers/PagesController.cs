deaf0850d94a9d5d48259ee3b9346e78691f49b8
yung lean <33080132+Michael23B@users.noreply.github.com>
2018-04-07 16:24:40 +1000

    'Edit Page' page added
    Does some checks for whether your editing the home page, if your title is unique etc.

--- Web/Areas/Admin/Controllers/PagesController.cs
+++ Web/Areas/Admin/Controllers/PagesController.cs
@@ -29,21 +29,19 @@ namespace Web.Areas.Admin.Controllers
         [HttpPost]
         public ActionResult AddPage(PageViewModel model)
         {
-            if (!ModelState.IsValid)
-            {
-                return View(model);
-            }
+            if (!ModelState.IsValid) return View(model);
 
             using (Db db = new Db())
             {
                 //If we have no slug, create one from the title otherwise clean up the provided slug
                 string slug = string.IsNullOrWhiteSpace(model.Slug) ? 
-                    model.Title.Replace(" ", ".").ToLower() 
+                    model.Title.Replace(" ", "-").ToLower() 
                     : 
-                    model.Slug.Replace(" ", ".").ToLower();
+                    model.Slug.Replace(" ", "-").ToLower();
 
                 //Check if database containts a page with this title or slug
-                if (db.Pages.Any(x => x.Title == model.Title) || db.Pages.Any(x => x.Slug == model.Slug))
+                //TODO: Add a custom attribute to do this check instead
+                if (db.Pages.Any(x => x.Title == model.Title) || db.Pages.Any(x => x.Slug == slug))
                 {
                     ModelState.AddModelError("", "Title or slug already exists!");
                     return View(model);
@@ -59,14 +57,79 @@ namespace Web.Areas.Admin.Controllers
                     Sorting = int.MaxValue  //when you add a page, it becomes the last page
                 };
 
-                //Save changes
+                //Save new page
                 db.Pages.Add(dto);
                 db.SaveChanges();
             }
 
-            TempData["message"] = $"Page {model.Title} added successfully!";
+            TempData["message"] = $"Page '{model.Title}' added successfully!";
 
             return RedirectToAction("Index");
+        }
+
+        public ActionResult EditPage(int id = 0)
+        {
+            PageViewModel page;
+
+            using (Db db = new Db())
+            {
+                PageDTO dto = db.Pages.Find(id);
+
+                if (dto == null) return Content($"Page with id: {id} does not exist!");
+
+                page = new PageViewModel(dto);
+            }
+
+            return View(page);
+        }
+
+        [HttpPost]
+        public ActionResult EditPage(PageViewModel model)
+        {
+            if (!ModelState.IsValid) return View(model);
+
+            using (Db db = new Db())
+            {
+                int id = model.Id;
+                string slug = "home";   //If model is not home page, slug will change; if it is, stay defaulted to home.
+
+                //Get slug
+                //TODO: reduce duplicated code from EditPage() methods. (GetSlug() method?)
+                if (model.Slug != "home")
+                {
+                        slug = string.IsNullOrWhiteSpace(model.Slug) ?
+                        model.Title.Replace(" ", "-").ToLower()
+                        :
+                        model.Slug.Replace(" ", "-").ToLower();
+                }
+
+                //Check for unique title and slug excluding itself
+                if (db.Pages.Where(x => x.Id != id).Any(x => x.Title == model.Title) || db.Pages.Where(x => x.Id != id).Any(x => x.Slug == slug))
+                {
+                    ModelState.AddModelError("", "Title or slug already exists!");
+                    return View(model);
+                }
+
+                //Update the db entry
+                PageDTO dto = db.Pages.Find(id);
+
+                dto.Title = model.Title;
+                dto.Slug = (dto.Slug == "home") ? "home" : slug;    //Malicious users can edit the form even if its readonly so we check here.
+                dto.Body = model.Body;
+                dto.HasSidebar = model.HasSidebar;
+
+                //Save changes
+                db.SaveChanges();
+            }
+
+            TempData["message"] = $"Page '{model.Title}' saved successfully!";
+
+            return RedirectToAction("Index");
+        }
+
+        private PageDTO GetSlug(PageDTO dto)
+        {
+            return dto;
         }
     }
 }
