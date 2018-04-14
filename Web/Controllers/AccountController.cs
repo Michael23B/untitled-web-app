cb00ce76d9c4a44aa47810b7de9dfbb92122ac34
yung lean <33080132+Michael23B@users.noreply.github.com>
2018-04-14 15:29:13 +1000

    login/logout form authentication

--- Web/Controllers/AccountController.cs
+++ Web/Controllers/AccountController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 using Microsoft.Ajax.Utilities;
 using Web.Models.Data;
 using Web.Models.ViewModels.Account;
@@ -25,6 +26,32 @@ namespace Web.Controllers
             return View();
         }
 
+        [HttpPost]
+        public ActionResult Login(LoginUserViewModel model)
+        {
+            if (!ModelState.IsValid) return View(model);
+
+            using (Db db = new Db())
+            {
+                if (db.Users.Any(x => x.Username == model.UserName && x.Password == model.Password))
+                {
+                    FormsAuthentication.SetAuthCookie(model.UserName, model.RememberMe);
+                    return Redirect(FormsAuthentication.GetRedirectUrl(model.UserName, model.RememberMe));
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Incorrect username or password!");
+                    return View(model);
+                }
+            }
+        }
+
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Login");
+        }
+
         [ActionName("create-account")]
         public ActionResult CreateAccount()
         {
@@ -35,24 +62,50 @@ namespace Web.Controllers
         [ActionName("create-account")]
         public ActionResult CreateAccount(UserViewModel model)
         {
-            if (!ModelState.IsValid) return View("create-account", model);
+            if (!ModelState.IsValid) return View("CreateAccount", model);
 
             if (model.Password != model.ConfirmPassword)
             {
                 ModelState.AddModelError("", "Passwords do not match!");
-                return View("create-account", model);
+                return View("CreateAccount", model);
             }
 
             using (Db db = new Db())
             {
                 if (db.Users.Any(x => x.Username == model.Username))
                 {
+                    ModelState.AddModelError("", $"Username {model.Username} is taken!");
+                    model.Username = "";
+                    return View("CreateAccount", model);
                 }
+
+                UserDTO userDTO = new UserDTO
+                {
+                    FirstName = model.FirstName,
+                    LastName = model.LastName,
+                    EmailAddress = model.EmailAddress,
+                    Username = model.Username,
+                    Password = model.Password
+                };
+
+                db.Users.Add(userDTO);
+                db.SaveChanges();
+
+                int id = userDTO.Id;
+                
+                UserRoleDTO userRoleDTO = new UserRoleDTO
+                {
+                    UserId = id,
+                    RoleId = 2
+                };
+
+                db.UserRoles.Add(userRoleDTO);
+                db.SaveChanges();
             }
 
-            TempData["message"] = "Username or password incorrect!";
+            TempData["message"] = "Registration successful";
 
-            return View("CreateAccount");
+            return Redirect("~/Account/Login");
         }
     }
 }
--- Web/Models/ViewModels/Account/LoginUserViewModel.cs
+++ Web/Models/ViewModels/Account/LoginUserViewModel.cs
@@ -1,15 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
+﻿using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Web;
 
 namespace Web.Models.ViewModels.Account
 {
     public class LoginUserViewModel
     {
         [Required]
+        [DisplayName("User name")]
         public string UserName { get; set; }
         [Required]
         public string Password { get; set; }
